939862ba0e604ae203f07e47a1a401d34d8139d7
rustamhasanau <103497080+rustamhasanau@users.noreply.github.com>
2022-04-22 19:41:37 +0300

    Add files via upload

---
+++ function caseInSwitch.js
@@ -0,0 +1,24 @@
+function caseInSwitch(val) {
+  let answer = "";
+  
+switch (val) {
+    case 1:
+      return "alpha";
+      break;
+    case 2:
+      return "beta";
+      break;
+    case 3:
+      return "gamma";
+      break;
+    case 4:
+      return "delta";
+      break;
+  }
+
+
+  
+  return answer;
+}
+
+caseInSwitch(1);
